fee21ed8de9c1b88553cbc725a3416369dfaba8a
PythonMonty <andrewnovosel90@gmail.com>
2024-06-12 11:41:53 -0400

    Added HTML setup for digital clock and changed class names

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Analog Clock
 const secondHand = document.querySelector(".second-hand");
 const minsHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
